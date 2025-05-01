26160a9df7cd8930d40fe9c563fa8ea628e1931f
Advik <advik.b@gmail.com>
2025-05-01 16:48:18 +0530

    Update User.java

--- src/main/java/dev/advik/wattpad/types/User.java
+++ src/main/java/dev/advik/wattpad/types/User.java
@@ -1,11 +1,15 @@
 package dev.advik.wattpad.types;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 
-public final class User {
+public class User {
+    @SerializedName(value = "fullname", alternate = {"name"})
     private final String name;
+
     private final String avatar;
+
+    @SerializedName(value = "username", alternate = {"name"})
     private final String username;
 
     public User(String name, String avatar, String username) {
@@ -14,20 +18,8 @@ public final class User {
         this.username = username;
     }
 
-    // Static method to create User from JSON
     public static User fromJson(JsonObject json) {
         Gson gson = new Gson();
-
-        if (json.has("fullname")) {
-            // Create a custom object manually if field names differ
-            return new User(
-                    json.get("fullname").getAsString(),
-                    json.get("avatar").getAsString(),
-                    json.get("name").getAsString()
-            );
-        }
-
-        // Fields match, we can deserialize directly
         return gson.fromJson(json, User.class);
     }
 
