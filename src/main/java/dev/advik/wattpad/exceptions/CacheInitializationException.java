84bb6aa391fd702f78967fed5087fd3e1ac39947
Advik <advik.b@gmail.com>
2025-05-01 17:09:46 +0530

    Create CacheInitializationException.java

---
+++ src/main/java/dev/advik/wattpad/exceptions/CacheInitializationException.java
@@ -0,0 +1,7 @@
+package dev.advik.wattpad.exceptions;
+
+public class CacheInitializationException extends WattpadException {
+    public CacheInitializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
