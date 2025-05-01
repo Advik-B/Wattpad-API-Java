a6f29c5b2793d5d7906acbc7d45f78c9c3885dcf
Advik <advik.b@gmail.com>
2025-05-01 19:01:53 +0530

    Create User.java

---
+++ src/main/java/dev/advik/wattpad/models/User.java
@@ -0,0 +1,38 @@
+package dev.advik.wattpad.models;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+
+public final class User {
+    @SerializedName(value = "fullname", alternate = {"name"})
+    private final String name;
+
+    private final String avatar;
+
+    @SerializedName(value = "username", alternate = {"name"})
+    private final String username;
+
+    public User(String name, String avatar, String username) {
+        this.name = name;
+        this.avatar = avatar;
+        this.username = username;
+    }
+
+    public static User fromJson(JsonObject json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, User.class);
+    }
+
+    // Optional getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
